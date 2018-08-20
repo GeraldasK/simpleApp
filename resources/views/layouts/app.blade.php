76dd7589c941a94bf28fcec10a7c68c236da3fb5
GeraldasK <gerk.kasp@icloud.com>
2018-08-20 15:05:56 +0300

    pridėjau viewsu

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>SimpApp</title>
+  </head>
+  <body>
+    @yield('content')
+
+    @include('inc.sidebar')
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
 });
