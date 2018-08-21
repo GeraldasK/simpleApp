6bb2106edf5709b683a89fc4df05f2e6791c1f9d
GeraldasK <gerk.kasp@icloud.com>
2018-08-21 20:20:50 +0300

    Pridejau dar viena controleri, isitraukiau irasus is Db

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,13 +3,13 @@
         <a class="navbar-brand" href="/simplewebsite/public/">simpApp</a>
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
+            <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
               <a class="nav-link" href="/simplewebsite/public/">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{Request::is('about') ? 'active' : ''}}">
               <a class="nav-link" href="/simplewebsite/public/about">About</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item {{Request::is('contacts') ? 'active' : ''}}">
               <a class="nav-link" href="/simplewebsite/public/contacts">Contacts</a>
             </li>
           </ul>
